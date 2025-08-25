c345373ff870b18e8161d5381c4c6e88ac7a6a86
Everton Ferreira <iseverton2024@gmail.com>
2025-08-25 14:02:50 -0300

    feat(data): add database context

---
+++ src/ServiceHub.Infrastructure/Data/ServiceHubDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ServiceHub.Domain.Entities;
+using ServiceHub.Domain.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceHub.Infrastructure.Data;
+
+public class ServiceHubDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+{
+    public ServiceHubDbContext(DbContextOptions<ServiceHubDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Provider> Providers { get; set; }
+    public DbSet<Service> Services { get; set; }
+    public DbSet<ServiceCategory> Categories { get; set; }
+    public DbSet<ServiceReview> ServiceReviews { get; set; }
+    public DbSet<CustomServiceRequest> CustomServiceRequests { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+
+
+
+}
