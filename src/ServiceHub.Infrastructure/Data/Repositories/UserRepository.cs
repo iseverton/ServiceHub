b50365b535ff4591f2a68feb6b81446b8e2a676b
Everton Ferreira <iseverton2024@gmail.com>
2025-08-30 16:48:32 -0300

    feat(repository) add user repository

---
+++ src/ServiceHub.Infrastructure/Data/Repositories/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServiceHub.Domain.Entities;
+using ServiceHub.Domain.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceHub.Infrastructure.Data.Repositories;
+
+public class UserRepository : BaseRepository<User>,IUserRepository
+{
+    public UserRepository(ServiceHubDbContext context) : base(context)
+    {
+    }
+
+    
+}
