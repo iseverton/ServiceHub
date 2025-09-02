ef980d291dc070202ed2aa5c8b7faba2267be558
Everton Ferreira <iseverton2024@gmail.com>
2025-09-02 15:58:37 -0300

    feat(log): add basic configuration log

--- src/ServiceHub.API/Program.cs
+++ src/ServiceHub.API/Program.cs
@@ -1,32 +1,60 @@
+using Serilog;
 using ServiceHub.Application.Handlers.Users;
 using ServiceHub.Infrastructure;
 
-var builder = WebApplication.CreateBuilder(args);
+// Configuracao do Serilog
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
 
-
-// Add services to the container.
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-// injecao de dependencias da infraestrutura
-builder.Services.AddInfrastructure(builder.Configuration);
-
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(RegisterUserCommandHandler).Assembly));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.MapOpenApi();
+    Log.Information("Starting web application");
+
+    var builder = WebApplication.CreateBuilder(args);
+    
+    builder.Host.UseSerilog();
+
+    // Add services to the container.
+    builder.Services.AddControllers();
+    // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+    builder.Services.AddOpenApi();
+
+    // injecao de dependencias da infraestrutura
+    builder.Services.AddInfrastructure(builder.Configuration);
+
+    builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(RegisterUserCommandHandler).Assembly));
+
+    
+    
+    
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
+    app.UseSerilogRequestLogging();
+    app.UseHttpsRedirection();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
+}
+catch (Exception ex)
+{
+
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
 
-app.UseHttpsRedirection();
 
-app.UseAuthentication();
-app.UseAuthorization();
 
-app.MapControllers();
-
-app.Run();
