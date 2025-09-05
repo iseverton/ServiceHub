a998d2a7fa33ae52c8911b9f73ecf5723507f2e2
Everton Ferreira <iseverton2024@gmail.com>
2025-09-05 14:26:34 -0300

    feat(repository): add service repository

---
+++ src/ServiceHub.Infrastructure/Data/Repositories/ServiceRepository.cs
@@ -0,0 +1,16 @@
+﻿using ServiceHub.Domain.Entities;
+using ServiceHub.Domain.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceHub.Infrastructure.Data.Repositories;
+
+public class ServiceRepository : BaseRepository<Service>, IServiceRepository
+{
+    public ServiceRepository(ServiceHubDbContext context) : base(context)
+    {
+    }
+}
