6d3870e1029ef3e47c51f428b8ae145e0c5a3203
Everton Ferreira <iseverton2024@gmail.com>
2025-09-05 22:24:34 -0300

    feat(entity): add Provider Schedule entity

---
+++ src/ServiceHub.Domain/Entities/ProviderSchedule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceHub.Domain.Entities;
+
+public class ProviderSchedule
+{
+    public Guid Id { get; set; }
+    public Guid ProviderId { get; set; }
+    public DayOfWeek DayOfWeek { get; set; }
+    public TimeSpan StartTime { get; set; }
+    public TimeSpan EndTime { get; set; }
+    public bool IsActive { get; set; }
+    public Provider? Provider { get; set; }
+
+    public ProviderSchedule(DayOfWeek dayOfWeek, TimeSpan startTime, TimeSpan endTime, Guid provider)
+    {
+        Id = Guid.NewGuid();
+        DayOfWeek = dayOfWeek;
+        StartTime = startTime;
+        EndTime = endTime;
+        ProviderId = provider;
+        IsActive = true;
+    }
+}
