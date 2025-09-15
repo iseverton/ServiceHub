69c81d126fab70f5f895a18653009d4766ff31e5
Everton Ferreira <iseverton2024@gmail.com>
2025-09-15 09:00:22 -0300

    feat(application): add DTOs for Identity authentication service responses

---
+++ src/ServiceHub.Application/DTOS/Auth/LoginResultDTO.cs
@@ -0,0 +1,11 @@
+﻿using ServiceHub.Domain.Entities.Identity;
+
+namespace ServiceHub.Application.DTOS.Auth;
+
+public class LoginResultDTO
+{
+    public bool Succeeded { get; set; }
+    public ApplicationUser? applicationUser { get; set; }
+    public List<string>? Errors { get; set; }
+
+}
---
+++ src/ServiceHub.Application/DTOS/Auth/ResultRegisterIdentityDTO.cs
@@ -0,0 +1,13 @@
+﻿using ServiceHub.Domain.Entities.Identity;
+
+namespace ServiceHub.Application.DTOS.Auth;
+
+/// <summary>
+///  DTO que encapsula o resultado do registro de um usuario de identidade
+/// </summary>
+public class ResultRegisterIdentityDTO
+{
+    public ApplicationUser? ApplicationUser { get; set; }
+    public List<string>? Errors { get; set; }
+    public bool Succeeded { get; set; }
+}
