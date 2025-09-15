403656cea67b5ae97ff5256e1384cb9c2a651570
Everton Ferreira <iseverton2024@gmail.com>
2025-09-15 09:05:05 -0300

    feat(user): add validation tests for user

---
+++ src/ServiceHub.Application/Commands/Users/Validations/RegisterUserCommandValidation.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceHub.Application.Commands.Users.Validations;
+
+public class RegisterUserCommandValidation : AbstractValidator<RegisterUserCommand>
+{
+    public RegisterUserCommandValidation()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required.")
+            .MinimumLength(3).WithMessage("Name must be at least 3 characters long.")
+            .MaximumLength(100).WithMessage("Name cannot exceed 100 characters.");
+
+        RuleFor(x => x.PhoneNumber)
+            .NotEmpty().WithMessage("Phone number is required.")
+            .Matches(@"^\+?[1-9]\d{1,14}$").WithMessage("Phone number is not valid.");
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("Email is not valid.");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required.")
+            .MinimumLength(6).WithMessage("Password must be at least 6 characters long.");
+        
+        RuleFor(x => x.ConfirmPassword)
+            .Equal(x => x.Password).WithMessage("Passwords do not match.");
+    }
+
+
+}
